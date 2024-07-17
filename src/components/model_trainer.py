3d2e23887f7580d0d227674f0eab43492183333c
Govin Weerasekera <govinbimsara@gmail.com>
2024-07-18 01:00:08 +0530

    Hyper parameters moved from model_trainer to config/mode_hyper_params.json, file path added to ModelTrainerConfig() and load_model_params() function was added to read the params from the .json

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 from dataclasses import dataclass
 
 from catboost import CatBoostRegressor
@@ -22,10 +23,20 @@ from src.utils import save_object,evaluate_models
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path: str=os.path.join('artifacts',"model.pkl")
+    model_params_file_path: str = os.path.join('config', "model_hyper_params.json")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
+        
+
+    def load_model_params(self):
+        try:
+            with open(self.model_trainer_config.model_params_file_path, 'r') as file:
+                params = json.load(file)
+            return params
+        except Exception as e:
+            raise CustomException(f"Error loading model parameters: {str(e)}", sys)
     
     def initiate_model_trainer(self,train_arr,test_arr):
         try:
@@ -45,42 +56,45 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            params={
-                "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
-                },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+            # params={
+            #     "Decision Tree": {
+            #         'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+            #         # 'splitter':['best','random'],
+            #         # 'max_features':['sqrt','log2'],
+            #     },
+            #     "Random Forest":{
+            #         # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                  
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
-                },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
-                }
-            }
+            #         # 'max_features':['sqrt','log2',None],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     "Gradient Boosting":{
+            #         # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+            #         'learning_rate':[.1,.01,.05,.001],
+            #         'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+            #         # 'criterion':['squared_error', 'friedman_mse'],
+            #         # 'max_features':['auto','sqrt','log2'],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     "Linear Regression":{},
+            #     "XGBRegressor":{
+            #         'learning_rate':[.1,.01,.05,.001],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     "CatBoosting Regressor":{
+            #         'depth': [6,8,10],
+            #         'learning_rate': [0.01, 0.05, 0.1],
+            #         'iterations': [30, 50, 100]
+            #     },
+            #     "AdaBoost Regressor":{
+            #         'learning_rate':[.1,.01,0.5,.001],
+            #         # 'loss':['linear','square','exponential'],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     }
+            # }
+
+            params = self.load_model_params()
+
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                              models=models,param=params)
             
